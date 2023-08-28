8c6e76336958125049aa20526713f437b2603469
tabee <bee.mario@gmail.com>
2023-08-28 16:11:13 +0000

    new option in widget

--- app/conversational_retrieval_agent.py
+++ app/conversational_retrieval_agent.py
@@ -1,7 +1,6 @@
 ''' 
 https://python.langchain.com/docs/use_cases/
 question_answering/how_to/conversational_retrieval_agents  '''
-import langchain
 from langchain.agents.agent_toolkits import (
     create_conversational_retrieval_agent, create_retriever_tool)
 from langchain.chat_models import ChatOpenAI
@@ -9,68 +8,79 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema.messages import SystemMessage
 from langchain.vectorstores import FAISS
 from langchain.callbacks import get_openai_callback
-from langchain.cache import SQLiteCache
 
 
-def ask_agent(query, openai_api_key, sys_path):
+def ask_agent(query, openai_api_key, sys_path, model='gpt-3.5-turbo-16k'):
     '''Display the answer to a question.'''
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-    new_db1 = FAISS.load_local(f'{sys_path}/data/vectorstores/eak_admin_ch_defaultdocs_faiss_index_4096',
-                            embeddings)
-    new_db2 = FAISS.load_local(f'{sys_path}/data/vectorstores/eak_admin_ch_defaultdocs_faiss_index_512',
-                            embeddings)
-    new_db3 = FAISS.load_local(f'{sys_path}/data/vectorstores/ch_ch_texts_faiss_index_4096',
-                            embeddings)
+    new_db1 = FAISS.load_local(
+        f'{sys_path}/data/vectorstores/eak_admin_ch_defaultdocs_faiss_index_4096',
+        embeddings)
+    new_db2 = FAISS.load_local(
+        f'{sys_path}/data/vectorstores/eak_admin_ch_defaultdocs_faiss_index_512',
+        embeddings)
+    # new_db3 = FAISS.load_local(f'{sys_path}/data/vectorstores/ch_ch_texts_faiss_index_4096',
+    #                         embeddings)
 
     new_db1.merge_from(new_db2)
-    new_db1.merge_from(new_db3)
+    # new_db1.merge_from(new_db3)
     new_db = new_db1
 
     retriever = new_db.as_retriever()
 
-
     tool = create_retriever_tool(
         retriever,
-        "eak_admin_website",
-        """Searches and returns documents regarding the eak_admin_website.
-        The eak_admin_website is the website of Eidg. Ausgleichskasse EAK."""""
+        "content_of_eak_website",
+        """
+        This tool is designed for an LLM that interacts with 
+        the content of the EAK website to retrieve documents. 
+        The EAK acts as a compensation fund for various federal entities. 
+        Its main responsibility is overseeing the implementation of 
+        the 1st pillar (AHV/IV) and the family compensation fund. 
+        The tool offers services related to:
+            - Insurance
+            - Contributions
+            - Employer regulations
+            - Pensions
+        Furthermore, it provides insights into family allowances and 
+        facilitates electronic data exchange with the EAK via connect.eak.
+        """
     )
     tools = [tool]
 
-
     system_message = SystemMessage(
-        content="""Du bist ein Experte der eak_admin_website
-        und beanwortest Fragen immer unter Angabe der Quelle.
-        Die Quelle ist die URL welche du im Tool eak_admin_website
-        als Antwort erhälst. Wenn du eine Antwort nicht weisst,
-        sage ich "keine Quelle, daher keine Antwort möglich".
-        Du darfst URLs auf keinen Fall erfinden. Nur URLs aus
-        eak_admin_website verwenden. Antworte immer auf deutsch.
+        content="""
+        You are an expert for the eak_admin_website and:
+        - Always answer questions citing the source.
+        - The source is the URL you receive as a response from the eak_admin_website tool.
+        - If you don't know an answer, state: "No source available, thus no answer possible".
+        - Never invent URLs. Only use URLs from eak_admin_website.
+        - Always respond in German.
         """
     )
 
-
     llm = ChatOpenAI(openai_api_key=openai_api_key,
-                    model='gpt-3.5-turbo-16k',
-                    temperature=0,
-                    n=3,
-                    verbose=True)
+                     model=model,
+                     temperature=0,
+                     n=10,
+                     verbose=True)
 
     agent_executor = create_conversational_retrieval_agent(
-        llm, tools, verbose=False, system_message=system_message)
-
+        llm, 
+        tools, 
+        verbose=False, 
+        system_message=system_message,
+        max_token_limit=3000) # heikel
+ 
     print(f"\nFrage: {query}")
     with get_openai_callback() as callback:
-        # geht: answer = agent_executor(query)
         answer = agent_executor({"input": query})
+        print(f"\nAntwort: {answer['output']}\n\n")
         print(f"Total Tokens: {callback.total_tokens}")
         print(f"Prompt Tokens: {callback.prompt_tokens}")
         print(f"Completion Tokens: {callback.completion_tokens}")
         print(f"Total Cost (USD): ${callback.total_cost}")
-    print(f"\nFrage: {query}")
-    print(f"\nAntwort: {answer['output']}")
-    print("============================================================\n")
     return answer
 
 
@@ -83,4 +93,6 @@ if __name__ == "__main__":
     ]
 
     for question in QUESTIONS:
-        ask_agent(question, openai_api_key=openai_api_key)
+        OPENAPI_API_KEY = "YOUR_API_KEY"
+        SYS_PATH = "YOUR_SYSTEM_PATH"
+        ask_agent(question, OPENAPI_API_KEY, SYS_PATH)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,43 @@
+""" Streamlit app for B3rn Zero Chat. """
 import streamlit as st
-from langchain.llms import OpenAI
 from app.question_optimizer_chain import optimize_question
 from app.conversational_retrieval_agent import ask_agent
 
 
-sys_path_local = '/workspaces/b3rn_zero_streamlit'
-sys_path_streamlit = '/mount/src/b3rn_zero_streamlit/'
-sys_path = sys_path_streamlit
+SYS_PATH_LOCAL = '/workspaces/b3rn_zero_streamlit'
+SYS_PATH_STREAMLIT = '/mount/src/b3rn_zero_streamlit/'
+SYS_PATH = SYS_PATH_LOCAL
 
 st.title('🤖 B3rn Zero Chat')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+openai_api_key = st.sidebar.text_input(
+    'OpenAI API Key',
+    value='',
+    type='password')
+optimize_input = st.sidebar.toggle('Optimizing question')
 
-
-def generate_response(input_text, openai_api_key=openai_api_key):
-    optimized_question = optimize_question(
-        input_text, openai_api_key=openai_api_key, sys_path=sys_path)
-    st.info(optimized_question)
-    answer = ask_agent(optimized_question,
-                       openai_api_key=openai_api_key, sys_path=sys_path)
+def generate_response(input_text):
+    """ Generate response from input text. """
+    if optimize_input:
+        input_text = optimize_question(
+            input_text,
+            openai_api_key,
+            SYS_PATH)
+        st.info(input_text)
+    answer = ask_agent(
+        input_text,
+        openai_api_key,
+        SYS_PATH)
     st.info(answer['output'])
 
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', '')
     submitted = st.form_submit_button('Submit')
+
+    if not optimize_input:
+        st.toast('Optimizing question deactivated.')
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text, openai_api_key=openai_api_key)
+        generate_response(text)
